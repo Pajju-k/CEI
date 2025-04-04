14497d57643983986b424a8fd74359534bda4775
Pajju-k <imprajwalsk@gmail.com>
2025-04-04 11:01:42 +0530

    save file

---
+++ calc.java
@@ -0,0 +1,21 @@
+class calc{
+    public static void main(String[] args){
+        int a=100,b=50;
+
+         int sum=a+b;
+             System.out.println("Addition is "+sum);
+
+         int diff=a-b;
+    System.out.println("Subtraction is "+diff);
+
+
+         int pro=a*b;
+    System.out.println("multiplication is "+pro);
+
+
+         int rem=a/b;
+    System.out.println("Division is "+rem);
+
+    }
+    
+}
